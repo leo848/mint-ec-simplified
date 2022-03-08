c341dfbe2bfa6e1011fedb3a7fd2ac7734c4cb06
Leo <leonardo.blume@gymnasium-essen-werden.de>
2022-03-08 21:30:09 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-main>
+      <MainPage />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue";
+import MainPage from "./components/MainPage.vue";
 
-@Component({
+export default Vue.extend({
+  name: "App",
+
   components: {
-    HelloWorld,
+    MainPage,
   },
-})
-export default class App extends Vue {}
-</script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "MainPage",
+
+  data: () => ({}),
+
+  methods: () => ({}),
+});
+</script>
+
+<template>
+  <v-container>
+    <h1>Willkommen bei mint-ec-simplified.</h1>
+    <h3>Bitte logge dich ein, um fortzufahren.</h3>
+  </v-container>
+</template>
