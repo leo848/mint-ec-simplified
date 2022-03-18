613d9269f74f7a1c250f874dc75b79c804bb29ee
Leo <leonardo.blume@gymnasium-essen-werden.de>
2022-03-18 21:39:16 +0100

    CRUD: mistyped "Category" for "Activity"

--- backend/crud.py
+++ backend/crud.py
@@ -105,7 +105,7 @@ def get_category(db: Session, category_id: int):
     return db.query(models.Category).filter(models.Category.id == category_id).first()
 
 def get_categories(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Activity).offset(skip).limit(limit).all()
+    return db.query(models.Category).offset(skip).limit(limit).all()
 
 def create_category(db: Session, category: schemas.CategoryCreate):
     new_category = models.Category(**category.dict())
