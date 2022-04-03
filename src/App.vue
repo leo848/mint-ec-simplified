26d4d8b1c3fe0af52974d3e75d6651b35aa3a5ea
Leo <leonardo.blume@gymnasium-essen-werden.de>
2022-04-03 19:02:25 +0200

    empty router, not yet used

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
-<script lang="ts">
-import Vue from "vue";
-import MainPage from "./components/MainPage.vue";
-
-export default Vue.extend({
-	name: "App",
-
-	components: {
-		MainPage,
-	},
-
-	data: () => ({
-		//
-	}),
-});
-</script>
-
 <template>
-	<MainPage />
+	<div id="app">
+		<router-view />
+	</div>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+	<MainPage />
+</template>
+
+<script>
+// @ is an alias to /src
+import MainPage from "@/components/MainPage.vue";
+
+export default {
+	name: "HomeView",
+	components: {
+		MainPage,
+	},
+};
+</script>
