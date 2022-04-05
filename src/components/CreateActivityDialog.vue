b7d07cd7841ed4a8e2c732b2ae96518e6376d37c
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-04-05 21:12:12 +0200

    date input on CAD

--- src/components/CreateActivityDialog.vue
+++ src/components/CreateActivityDialog.vue
@@ -5,7 +5,9 @@ export default Vue.extend({
 	data: () => ({
 		show: false,
 		valid: true,
-		data: { title: "", description: "" },
+		activeDatePicker: "YEAR",
+		data: { title: "", description: "", date: null },
+		dateMenu: false,
 		rules: {
 			title: [
 				(v: string) => !!v || "Bitte gib einen Titel ein.",
@@ -88,6 +90,41 @@ export default Vue.extend({
 									clearable
 									clear-icon="mdi-close"
 								></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="6">
+								<v-menu
+									ref="dateMenu"
+									v-model="dateMenu"
+									:close-on-content-click="false"
+									offset-y
+									min-width="auto"
+								>
+									<template v-slot:activator="{ on, attrs }">
+										<v-text-field
+											v-model="data.date"
+											label="Zeitraum"
+											prepend-icon="mdi-calendar"
+											readonly
+											v-bind="attrs"
+											v-on="on"
+										></v-text-field>
+									</template>
+									<v-date-picker
+										v-model="data.date"
+										:active-picker.sync="activeDatePicker"
+										no-title
+										scrollable
+										language="de"
+										:max="
+											new Date(
+												Date.now() - new Date().getTimezoneOffset() * 60000,
+											)
+												.toISOString()
+												.substr(0, 10)
+										"
+										min="1950-01-01"
+									></v-date-picker>
+								</v-menu>
 							</v-col>
 						</v-row>
 					</v-container>
