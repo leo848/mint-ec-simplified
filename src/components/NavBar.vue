d535d103a17d4c7bc7640e054ccb603a0aebca56
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-05-11 19:29:42 +0200

    dark mode toggle
    (this was very easy)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,9 +4,25 @@ import Vue from "vue";
 export default Vue.extend({
 	name: "NavBar",
 	data: () => ({}),
+	created() {
+		const darkMode = localStorage.getItem("darkMode");
+		if (darkMode === null) {
+			this.$vuetify.theme.dark = false;
+			this.toggleMode();
+		} else {
+			this.$vuetify.theme.dark = JSON.parse(darkMode);
+		}
+	},
 	methods: {
 		logout() {
 			localStorage.removeItem("token");
+		},
+		toggleMode() {
+			this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+			localStorage.setItem(
+				"darkMode",
+				JSON.stringify(this.$vuetify.theme.dark),
+			);
 		},
 	},
 });
@@ -18,5 +34,7 @@ export default Vue.extend({
 			@click="$emit('drawerVisibilityToggle')"
 		></v-app-bar-nav-icon>
 		<v-toolbar-title>{{ $route.meta.title }}</v-toolbar-title>
+		<v-spacer />
+		<v-btn icon @click="toggleMode"><v-icon>mdi-brightness-6</v-icon></v-btn>
 	</v-app-bar>
 </template>
